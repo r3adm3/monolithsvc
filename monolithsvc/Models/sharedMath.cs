10962509ebad3e9cf58140407d8aab608be6356c
Adrian Farnell <adrianjfarnell@gmail.com>
2019-02-23 09:38:12 +0000

    saved sharedMath.cs

--- monolithsvc/Models/sharedMath.cs
+++ monolithsvc/Models/sharedMath.cs
@@ -9,6 +9,16 @@ namespace monolithsvc.Models
         {
             return a+b;
         }
+
+        public int multiplyTwoNumbers(int a, int b)
+        {
+            return a*b;
+        }
+
+        public int minusTwoNumbers(int a, int b)
+        {
+            return a-b;
+        }
     }
 
 
