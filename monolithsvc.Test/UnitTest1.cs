dec2a6a6977174b94fb9365c390533962bc9aafc
Adrian Farnell <adrianjfarnell@gmail.com>
2019-02-23 08:32:35 +0000

    setup framework for autotesting

---
+++ monolithsvc.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace monolithsvc.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ monolithsvc/Models/sharedMath.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace monolithsvc.Models
+{
+
+    public class sharedMath
+    {
+        public int addTwoNumbers (int a, int b)
+        {
+            return a+b;
+        }
+    }
+
+
+}
