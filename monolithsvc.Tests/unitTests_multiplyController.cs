e282cead01841c7edcd448d9c03f5ce88075fbdb
Adrian Farnell <adrianjfarnell@gmail.com>
2019-02-25 21:52:57 +0000

    Unit tests for each of the controllers now up and running

---
+++ monolithsvc.Tests/unitTests_multiplyController.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using monolithsvc.Controllers;
+using Microsoft.Extensions.Configuration; 
+
+namespace monolithsvc.Tests
+{
+    [TestClass]
+    public class unitTests_multiplyController
+    {
+        [TestMethod]
+        public void multiplyControllerTwoMultiplyOneEqualsTwo()
+        {
+            var controller = new multiplyController();
+
+            var result = controller.Get(2,1);
+
+            var actualResult = result.Value;
+
+            Assert.AreEqual(2, actualResult);
+
+        }
+
+    }
+}
