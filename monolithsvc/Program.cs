d1c29eaca7f40aa920df029839094281505dd80a
Adrian Farnell <adrianjfarnell@gmail.com>
2019-03-01 22:08:27 +0000

    Simple change to force the CI to kick off

--- monolithsvc/Program.cs
+++ monolithsvc/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+//commit to force a change
 namespace monolithsvc
 {
     public class Program
