0a364816ceaaff4bb6c47e14a61955714fdeda2a
Adrian Farnell <adrianjfarnell@gmail.com>
2020-02-04 15:45:17 +0000

    comment change to invoke CICD

--- monolithsvc/Program.cs
+++ monolithsvc/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-//commit to force a change
+//commit to force a change (x2 4/2)
 namespace monolithsvc
 {
     public class Program
